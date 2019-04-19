6d0e960ccd4d75e7c0750497bbf431668d1a05ec
Keith Burgie <krburgie@gmail.com>
2019-04-19 05:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function dwarfRollCall(dwarves) {
+  let numberedList = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    numberedList.push(`${i+1}. ${dwarves[i]}`);
+  }
+  return `${numberedList.join(' ')} `;
 }
 
+
+//let m = ["earth", "wind", "fire", "water", "heart"];
+
 function summonCaptainPlanet(planeteerCalls){
+  let elements = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    elements.push(`${planeteerCalls[i].toUpperCase()}!`);
+  }
+  return elements;
 }
+
+//summonCaptainPlanet(m);
+
 
 function longPlaneteerCalls(words) {
 }
+
 
 function findTheCheese (foods) {
 }
