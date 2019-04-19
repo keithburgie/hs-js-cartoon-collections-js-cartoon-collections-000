1c5e98ec71b1907fb11933ca1161faffc9181c35
Keith Burgie <krburgie@gmail.com>
2019-04-19 06:34:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+  let numberedList = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    numberedList.push(`${i+1}. ${dwarves[i]}`);
+  }
+  return `${numberedList.join(' ')} `;
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  let elements = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    elements.push(`${planeteerCalls[i].toUpperCase()}!`);
+  }
+  return elements;
 }
+
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
+
 function findTheCheese (foods) {
+  const cheeses = ["cheddar", "gouda", "camembert"];
+  
+  for (let i = 0; i < foods.length; i++) {
+    if (cheeses.indexOf(foods[i]) > -1) {
+      return foods[i];
+    }
+  }
+  return "no cheese!";
 }
